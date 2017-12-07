0bc01cd36516fd561894e642cadbe1b9b147fbbc
clay <clay@catclay.cn>
2017-12-08 00:32:52 +0800

    init source

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
+var login = require('./routes/login');
 var users = require('./routes/users');
 var task = require('./routes/task');
 
@@ -24,6 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+app.use('/login', login);
 app.use('/users', users);
 app.use('/task', task);
 
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/', function (req, res, next) {
+    var user = req.body;
+    var name = user.userName;
+});
+
 module.exports = router;
