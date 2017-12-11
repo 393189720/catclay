2127a4303d72aa5e1f9814a90de26eea4b5df465
liyan <liyan@beyondcent.com>
2017-12-11 18:26:36 +0800

    结构修改

---
+++ app/models/web/taskNoticeModel.js
@@ -0,0 +1,6 @@
+var TaskNoticeSchema = new Schema({
+    //--基本公告信息， index表示在mongodb中会建立索引
+    //--unique: true 唯一性设置
+    content: { type: String },
+    // createTime: { type: Date }
+});
--- config/mongodb.sh
+++ config/mongodb.sh
@@ -1,3 +1,14 @@
 #表
-use users
-user taskNotic
+#use catclay  \创建一个叫chihuo的数据库
+#db.createCollection('users') \创建一个集合
+#db.users.insert({'name':'admin','password':'111'}) \给users集合添加一个文档。
+#db.users.find() \查询你添加的文档
+use catclay
+db.createCollection('users')
+db.users.insert({'content”:''})
+
+db.createCollection("task")
+db.users.insert({'content”:''})
+
+db.createCollection('taskNotice')
+db.users.insert({'content”:''})
